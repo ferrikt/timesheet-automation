ae23c3900c83ba56fb5a6b4a2eea478ead3b3058
Natallia Skviarniuk <natallia.skviarniuk@iag.com.au>
2021-03-10 10:00:34 +1100

    remove leftover

--- cypress/integration/examples/fieldglass.js
+++ cypress/integration/examples/fieldglass.js
@@ -10,9 +10,9 @@ describe('Timesheet filling against fieldglass', () => {
         });
     })
     
-    it('goes to fieldglass website and waits for the input to be visible', () => {
+    it('goes to fieldglass website', () => {
         cy.visit(config.url);
-        cy.get('#usernameId_new').click();
+       // cy.get('#usernameId_new').click();
     });
 
     it('fills in the user credentials', () => {
@@ -28,30 +28,14 @@ describe('Timesheet filling against fieldglass', () => {
         cy.get('#passwordId_new').type(config.creds.password);
         cy.get("#passwordId_new").type('{enter}');
     })
-    it('fills in the timesheet for the week', () => {
+    it('fills in the timesheet for the week - 5 days', () => {
 
         const getClassNames = () => {
-            // const d = new Date();
-            // const year = d.getFullYear();
-            // const month = (d.getMonth()+1)<10 ? `0${(d.getMonth()+1)}`: d.getMonth()+1;
-           
-
-            // let dDay = Number(config.timesheet.startDay);
-            // const result = [];
-            // for(let i=0;i<5;i++) {
-            //     const day = dDay<10 ? `0${dDay}`: dDay;
-            //     result.push(`${year}${month}${day}`);
-            //     dDay+=1;
-            // }
-            // return result;
-
             const result = [];
 
             let currentDate = startOfWeek(new Date(), {weekStartsOn: 1});
             let currentDay = getDate(currentDate);
            
-           
-
             for(let i=0;i<5;i++) {
                 const day = currentDay<10 ? `0${currentDay}`: currentDay;
                 const month = (getMonth(currentDate)+1)<10 ? `0${(getMonth(currentDate)+1)}`: getMonth(currentDate)+1;
@@ -72,8 +56,8 @@ describe('Timesheet filling against fieldglass', () => {
 
         cy.get("th.captionBig").contains("Day").first().click();
     })
-    it("clicks on continue later button", () => {
-       cy.get("input#fgTSSubmit").click();
+    it("clicks on Submit button", () => {
+       cy.get('#fgTSSubmit').click();
        cy.wait(10000)
     })
 })
